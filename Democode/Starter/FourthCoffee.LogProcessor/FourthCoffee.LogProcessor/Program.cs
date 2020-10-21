78a981b00b065efb77585f66fa3166917d89962d
Luismi1601 <Luismi16@gmail.com>
2020-10-21 16:36:24 +0200

    commit

---
+++ Democode/Starter/FourthCoffee.LogProcessor/FourthCoffee.LogProcessor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthCoffee.LogProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO 09: Replace [Repository Root] with your folder path 
+            var logLocator = new LogLocator(@"C:\Curso\20483\Luismi\Tareas\Mod06_T20483\Democode\Data\Logs\");
+            var logCombiner = new LogCombiner(logLocator);
+
+            logCombiner.CombineLogs(@"C:\Curso\20483\Luismi\Tareas\Mod06_T20483\Democode\Data\Logs\CombinedLog.txt");
+
+        }
+    }
+}
